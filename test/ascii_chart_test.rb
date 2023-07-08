49b60f67f1abe20303480b0b521e7ac1c4303446
Karl Heitmann <khf1988@gmail.com>
2023-07-07 22:19:22 -0400

    Test array with same elements. Test fails

--- test/ascii_chart_test.rb
+++ test/ascii_chart_test.rb
@@ -6,4 +6,14 @@ class AsciiChartTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::AsciiChart::VERSION
   end
+
+  def test_writes_array_same_numbers
+    input_array = [4, 4, 4, 4.00, 4, 4, 4, 4, 4, 4]
+    assert_equal(AsciiChart.plot(input_array), '    4.00  ┼--------- ')
+  end
+
+  def test_writes_array_same_numbers_and_height_provided
+    input_array = [4, 4, 4, 4.00, 4, 4, 4, 4, 4, 4]
+    assert_equal(AsciiChart.plot(input_array, { height: 10 }), '    4.00  ┼--------- ')
+  end
 end
